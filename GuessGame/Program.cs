cb0e9a476555f462c136a3f602df291103ed63e2
Ahmad Bakare <65125857+adelekeahmad@users.noreply.github.com>
2022-08-21 00:29:05 -0700

    Add project files.

---
+++ GuessGame/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace GuessGame;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+        string secretWord = "Chelsea";
+        string guess = "";
+        while(guess != secretWord)
+        {
+            Console.Write("Who wins today's match: ");
+            guess = Console.ReadLine();
+    
+        }
+        //if condition does not hold
+        Console.Write("you win the game!");
+
+    }
+ 
+}
