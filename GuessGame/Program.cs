804a32f679cbd6cf10c47ac70dbe4b591bde9581
Ahmad Bakare <65125857+adelekeahmad@users.noreply.github.com>
2022-08-21 01:42:01 -0700

    Update Program.cs
    Included guess count, guess limit and a boolean statement which is  out of guess in the code to factor in the conditions that hold if the user loses or wins the game.

--- GuessGame/Program.cs
+++ GuessGame/Program.cs
@@ -7,15 +7,35 @@ class Program
         //Console.WriteLine("Hello, World!");
         string secretWord = "Chelsea";
         string guess = "";
-        while(guess != secretWord)
+        int guessCount = 0;
+        int guessLimit = 3;
+        bool outOfGuesses = false;
+        while(guess != secretWord && !outOfGuesses)
+          {
+
+        if (guessCount < guessLimit) 
         {
             Console.Write("Who wins today's match: ");
             guess = Console.ReadLine();
-    
+            guessCount++;
+        }   
+        
+        else
+        {
+             outOfGuesses = true;
         }
-        //if condition does not hold
-        Console.Write("you win the game!");
-
-    }
+                                       
+        }
+        if (outOfGuesses) 
+        {
+            Console.Write("you lose!");
+        }
+        else 
+        { 
+            //if condition does not hold
+        Console.Write("you win!");
+        }
+        Console.ReadLine();
+    }   
  
 }
